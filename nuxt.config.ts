1934acbfbdf7044d9a2166b44ef9949478a34c3f
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-03-16 14:27:30 -0300

    Added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 import { NuxtConfig } from '@nuxt/types'
 
 const config: NuxtConfig = {
-	buildModules: ['@nuxt/typescript-build'],
+	buildModules: ['@nuxt/typescript-build', '@nuxtjs/tailwindcss'],
 	components: true,
 	srcDir: 'src',
 	target: 'static',
