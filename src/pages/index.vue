f2c87672e3a791b223a2559dbd4e101eaa82c252
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-04-02 09:18:10 -0300

    added motion when scrolling

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,6 +4,7 @@
 			<div class="container max-w-screen-lg">
 				<div
 					class="bg-primary flex flex-col shadow-md text-center space-y-4 px-8 md:px-16 py-16 rounded-xl text-white"
+					v-motion-pop-bottom
 				>
 					<div class="flex flex-col space-y-2">
 						<h2 class="font-bold text-4xl md:text-5xl">
@@ -33,6 +34,7 @@
 				<Heading
 					title="What we do?"
 					paragraph="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut fugit, a voluptatum excepturi culpa ducimus praesentium vel ratione quo quae?"
+					v-motion-pop-bottom
 				/>
 				<div class="grid grid-cols-12 gap-y-8 gap-x-0 md:gap-x-8">
 					<div
@@ -40,7 +42,7 @@
 						v-for="item in [0, 1, 2, 3]"
 						:key="item"
 					>
-						<Feature />
+						<Feature v-motion-pop-bottom />
 					</div>
 				</div>
 			</div>
@@ -51,6 +53,7 @@
 				<Heading
 					title="Our products"
 					paragraph="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut fugit, a voluptatum excepturi culpa ducimus praesentium vel ratione quo quae?"
+					v-motion-pop-bottom
 				/>
 				<div class="grid grid-cols-12 gap-y-8 gap-x-0 md:gap-x-8">
 					<div
@@ -58,7 +61,7 @@
 						v-for="item in [0, 1, 2, 3, 4, 5]"
 						:key="item"
 					>
-						<Product />
+						<Product v-motion-pop-bottom />
 					</div>
 				</div>
 			</div>
@@ -72,12 +75,14 @@
 					>
 						<div
 							class="bg-gray-200 shadow-md flex h-64 items-center justify-center rounded-xl w-full"
+							v-motion-pop-bottom
 						>
 							Google Maps
 						</div>
 					</div>
 					<div
 						class="col-span-12 row-start-1 md:col-span-6 text-center md:text-left items-center md:items-start flex flex-col justify-center space-y-4"
+						v-motion-pop-bottom
 					>
 						<h2 class="font-bold text-4xl">Where we are?</h2>
 						<p class="leading-8 text-lg">
@@ -96,6 +101,7 @@
 				<div class="grid grid-cols-12 gap-y-8 gap-x-0 md:gap-x-8">
 					<div
 						class="col-span-12 md:col-span-6 text-center md:text-left items-center md:items-start flex flex-col justify-center space-y-4"
+						v-motion-pop-bottom
 					>
 						<h2 class="font-bold text-4xl">Contact</h2>
 						<p class="leading-8 text-lg">
@@ -148,7 +154,7 @@
 						</div>
 					</div>
 					<div class="col-span-12 md:col-span-6">
-						<ContactForm />
+						<ContactForm v-motion-pop-bottom />
 					</div>
 				</div>
 			</div>
