53890d915bb4ae642d12c1c1b1424a220b0781e3
Lautaro Pereyra <dev.lautaropereyra@gmail.com>
2021-09-05 09:54:35 -0300

    update appearance of scroll indicator

--- src/components/ui/ScrollIndicator.vue
+++ src/components/ui/ScrollIndicator.vue
@@ -1,8 +1,11 @@
 <template>
 	<div
-		class="w-screen h-1 bg-gray-200 dark:bg-gray-900 fixed top-0 left-0 z-30"
+		class="w-screen h-0.5 bg-gray-200 dark:bg-gray-700 fixed top-0 left-0 z-30"
 	>
-		<div class="bg-primary-600 h-full w-0" ref="progress"></div>
+		<div
+			class="bg-primary-600 dark:bg-primary-400 h-full w-0"
+			ref="progress"
+		></div>
 	</div>
 </template>
 
@@ -10,7 +13,7 @@
 import Vue from 'vue'
 export default Vue.extend({
 	mounted() {
-		document.onscroll = (event) => {
+		document.onscroll = event => {
 			if (document.scrollingElement) {
 				const progress = this.$refs['progress'] as HTMLDivElement
 				const value =
